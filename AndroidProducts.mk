67742833ec2352ef4ac22c52910067b2816c60bd
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-01-09 12:11:24 +0800

    fuxi: Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_fuxi.mk
+
+COMMON_LUNCH_CHOICES := twrp_fuxi-eng
